661552b210aabddd8d3cff445165a44a9fa54606
AmshenShanu07 <amshen.shanu07.ya@gmail.com>
2026-01-14 19:14:26 +0530

    feat: fix prisma module issue

--- src/core/whatsapp/whatsapp.module.ts
+++ src/core/whatsapp/whatsapp.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WhatsappService } from './whatsapp.service';
 import { WhatsappController } from './whatsapp.controller';
+import { PrismaService } from 'src/services/prisma.service';
 
 @Module({
   controllers: [WhatsappController],
-  providers: [WhatsappService],
+  providers: [WhatsappService, PrismaService],
 })
 export class WhatsappModule {}
