3b2e6e48369f0166c6aa0882d9f120b2d468ff3e
AmshenShanu07 <amshen.shanu07.ya@gmail.com>
2026-01-18 13:54:52 +0530

    feat: added order flow

--- src/core/whatsapp/whatsapp.service.ts
+++ src/core/whatsapp/whatsapp.service.ts
@@ -45,6 +45,15 @@ export class WhatsappService {
         return 'This action only accepts text messages';
       }
 
+      if(type == 'order') {
+        // {"object":"whatsapp_business_account","entry":[{"id":"1883543198870590","changes":[{"value":{"messaging_product":"whatsapp","metadata":{"display_phone_number":"917559903011","phone_number_id":"769195252945723"},"contacts":[{"profile":{"name":"sh4nu"},"wa_id":"917012670512"}],"messages":[{"from":"917012670512","id":"wamid.HBgMOTE3MDEyNjcwNTEyFQIAEhgUM0FEODI1RTM0NkZFQ0E5NUU3OUQA","timestamp":"1768724525","type":"order","order":{"catalog_id":"1978442546302613","text":"","product_items":[{"product_retailer_id":"cmk9dpnel0005ufrl14mn4c7l","quantity":1,"item_price":1,"currency":"INR"},{"product_retailer_id":"cmk9dmwzi0001ufrlnzf52jc1","quantity":1,"item_price":5,"currency":"INR"},{"product_retailer_id":"cmk9dr1hv0007ufrlqf4mayq6","quantity":1,"item_price":7.5,"currency":"INR"}]}}]},"field":"messages"}]}]}
+        await this.createOrder(
+          data.entry[0].changes[0].value.messages[0].from, 
+          data.entry[0].changes[0].value.messages[0].order.product_items
+        );
+        return 'Order created successfully';
+      }
+
 
 
   
